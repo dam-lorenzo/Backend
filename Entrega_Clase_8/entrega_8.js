ceee8956f4c9ee2fb498693d73abcff79c25a2c3
damian <dam.lorenzo@hotmail.com>
2022-03-14 22:27:08 -0300

    Create routers.js for entrega_8.js

--- Entrega_Clase_8/entrega_8.js
+++ Entrega_Clase_8/entrega_8.js
@@ -1,19 +1,12 @@
 //      Requires
 const express       = require('express')
-const { Router }    = express
 const { print }     = require('print')
-const { apiManager} = require('apiManager')
+const { router_products } = require('router_products')
 
 //      Server Config
 const app               = express()
 const PORT              = 8080
-const router_products   = Router()
-router_products.use(express.urlencoded({extended: true}))
-router_products.use(express.json())
 app.use(express.static('Clase_8/public'))
-
-//      Global variables
-const api = new apiManager()
 
 //      Server connection
 const server = app.listen( PORT, () => {
@@ -22,31 +15,6 @@ const server = app.listen( PORT, () => {
 
 server.on('error', error => print(`Error en el servidor ${error}`))
 
+//      Server routes
+
 app.use('/api/products', router_products)
-
-//      Routers
-
-router_products.get('/', (req, res) => {
-    res.json(api.getAll())
-} )
-
-router_products.get('/:id', (req, res) => {
-    const _id = req.params.id
-    res.json(api.getByID(_id))
-} )
-
-router_products.post('/', (req, res) => {
-    const item = req.body
-    res.json(api.addItem(item))
-} )
-
-router_products.put('/:id', (req, res) => {
-    const _id = req.params.id
-    const new_item = req.body
-    res.json(api.updateItem(_id, new_item))
-} )
-
-router_products.delete('/:id', (req, res) => {
-    const _id = req.params.id
-    res.json(api.deleteItem(_id))
-} )
