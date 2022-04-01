381f5a0870e2c0d5088b53a2ced83e7656272479
damian <dam.lorenzo@hotmail.com>
2022-03-31 22:37:14 -0300

    Create Entrega_12

---
+++ Entrega_Clase_12/index.js
@@ -0,0 +1,46 @@
+//      Requires
+const express           = require('express')
+const { print }         = require('print')
+const { Server: IOServer }      = require('socket.io')
+const { Server: HttpServer }    = require('http')
+
+//      Server Config
+const app               = express()
+const PORT              = 8080
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(express.static(__dirname + '/public'))
+const httpServer        = new HttpServer(app)
+const io                = new IOServer(httpServer)
+
+app.use('/', (req, res) => {
+    res.sendFile('index.html')
+} )
+
+//      Global variables
+const messages = []
+const products = [{name: 'Notebook', price: '700'}]
+
+//      Server connection
+
+httpServer.listen( PORT, () => {
+    print(`Servidor HTTP excuchando en el puerto ${PORT}`)
+} )
+
+//      Socket
+
+io.on('connection', (socket) => {
+    print('User connected')
+    socket.emit('items', products)
+    socket.on('item', (data) => {   
+        products.push(data)
+        print(products)
+        io.sockets.emit('items', products)
+    } )
+    socket.emit('messages', messages)
+    socket.on('new-message', (message) => {
+        messages.push(message)
+        io.sockets.emit('messages', messages)
+    } )
+    
+})
---
+++ Entrega_Clase_12/public/js/main.js
@@ -0,0 +1,51 @@
+const socket = io.connect()
+
+socket.on('items', (data) => {
+    if (data.length > 0) {
+        renderRow(data) 
+    }
+} )
+
+function renderRow(data) {
+    let html = '<tr style="color: yellow;"> <th>Item</th> <th>Precio</th> </tr>'
+    html += data.map((item, index) => {
+        return(`<tr>
+                    <td> ${item.name}</td>
+                    <td> ${item.price} </td>
+                </tr>`)
+    }).join(" ");
+    document.getElementById('table').innerHTML = html;
+}
+
+function addItem(e) {
+    const name = document.getElementById('name').value
+    const price = document.getElementById('price').value
+    const item = {
+        name:  name,
+        price: price
+    };
+    alert(name, price)
+    socket.emit('item', item);
+    return false;
+}
+socket.on('messages', function(data) { 
+    render(data)
+ })
+
+function render(data) {
+    const html = data.map((elem, index) => {
+        return(`<div>
+            <strong>${elem.user}</strong>:
+            <em>${elem.text}</em> </div>`)
+    }).join(" ");
+    document.getElementById('messages').innerHTML = html;
+}
+
+function sendMessage(e) {
+    const message = {
+        user: document.getElementById('user').value,
+        text: document.getElementById('text').value
+    };
+    socket.emit('new-message', message);
+    return false;
+}
