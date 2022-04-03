d6750a23e138b98410c2040afcbcf1cd86b767f5
damian <dam.lorenzo@hotmail.com>
2022-04-03 19:33:06 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+//      Requires
+const express               = require('express')
+const { print } = require("print")
+const { router_products } = require("router_products")
+
+//      Server Config
+const app               = express()
+const PORT              = 8080
+app.use(express.static('Clase_8/public'))
+
+//      Server connection
+const server = app.listen( PORT, () => {
+    print(`Servidor Express excuchando en el puerto ${server.address().port}`)
+} )
+
+server.on('error', error => print(`Error en el servidor ${error}`))
+
+//      Server routes
+
+app.use('/api/products', router_products)
