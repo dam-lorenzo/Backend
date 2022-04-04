3b59899632dd8d3b67ce6f2d4092b4e73729a1c9
damian <dam.lorenzo@hotmail.com>
2022-04-03 22:46:31 -0300

    Create router_products.js

---
+++ src/controllers/router_products.js
@@ -0,0 +1,42 @@
+const express       = require('express')
+const { Router }    = express
+const { Container } = require('apiManager')
+
+//      Global variables
+
+const api = new Container('products')
+
+//      Router
+
+const router_products   = Router()
+router_products.use(express.urlencoded({extended: true}))
+router_products.use(express.json())
+
+router_products.get('/', (req, res) => {
+    res.json(api.getAll())
+} )
+
+router_products.get('/:id', (req, res) => {
+    const _id = req.params.id
+    res.json(api.getByID(_id))
+} )
+
+router_products.post('/', (req, res) => {
+    const item = req.body
+    res.json(api.addItem(item))
+} )
+
+router_products.put('/:id', (req, res) => {
+    const _id = req.params.id
+    const new_item = req.body
+    res.json(api.updateItem(_id, new_item))
+} )
+
+router_products.delete('/:id', (req, res) => {
+    const _id = req.params.id
+    res.json(api.deleteItem(_id))
+} )
+
+
+
+module.exports = { router_products }
