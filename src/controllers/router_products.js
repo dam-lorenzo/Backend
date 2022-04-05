11acc43aae271cbb7a5e5c7155f993432b1f6c3f
damian <dam.lorenzo@hotmail.com>
2022-04-04 23:38:18 -0300

    Update royter_products.js

--- src/controllers/router_products.js
+++ src/controllers/router_products.js
@@ -5,6 +5,7 @@ const { Container } = require('apiManager')
 //      Global variables
 
 const api = new Container('products')
+let is_admin = false
 
 //      Router
 
@@ -23,20 +24,72 @@ router_products.get('/:id', (req, res) => {
 
 router_products.post('/', (req, res) => {
     const item = req.body
-    res.json(api.addItem(item))
+    if ('admin' in item){
+        is_admin = true
+        delete item.admin
+    }
+    if (is_admin){
+        const missing_keys = validateItem(item)
+        console.log(missing_keys.length > 0)
+        const cond = missing_keys.length > 0
+        // if (missing_keys.lenght > 0) {   // Por alguna razon esta condicion se evalua siempre como false y me va siempre al else. Por eso termine usando el cond
+        if (cond) {
+            res.json({'error': `el item cargado no tiene la key ${missing_keys.join(', ')}`})
+        }
+        else {
+            res.json(api.addItem(item))
+        }
+    }
+    else {
+        res.json({'error': -2, 'description': "ruta '/api/products/' metodo POST no autorizada"})
+    }
+    is_admin = false
 } )
 
 router_products.put('/:id', (req, res) => {
-    const _id = req.params.id
     const new_item = req.body
-    res.json(api.updateItem(_id, new_item))
+    if ('admin' in new_item){
+        is_admin = true
+        delete new_item.admin
+    }
+    if (is_admin){
+        const _id = req.params.id
+        res.json(api.updateItem(_id, new_item))
+    }
+    else {
+        res.json({'error': -2, 'description': "ruta '/api/products/:id' metodo PUT no autorizada"})
+    }
+    is_admin = false  
 } )
 
 router_products.delete('/:id', (req, res) => {
-    const _id = req.params.id
-    res.json(api.deleteItem(_id))
+    const body = req.body
+    if ('admin' in body){
+        is_admin = true
+        delete body.admin
+    }
+    if (is_admin){
+        const _id = req.params.id
+        res.json(api.deleteByID(_id))
+    }
+    else {
+        res.json({'error': -2, 'description': "ruta '/api/products/:id' metodo DELETE no autorizada"})
+    }
+    is_admin = false 
 } )
 
+//      Helpers
+
+function validateItem(item){
+    const keys = ['name', 'description', 'code', 'image', 'price', 'stock']
+    const missing_keys = []
+    for (let key of keys) {
+        if (!(key in item)) {
+            missing_keys.push(key)
+        }
+    }
+    return missing_keys
+}
 
 
 module.exports = { router_products }
